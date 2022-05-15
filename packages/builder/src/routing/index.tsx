678a00895f18161bd5b8b891185c90f4b134a082
Vitalii <akumuliation@gmail.com>
2022-05-15 20:39:52 +0300

    add libraries

---
+++ packages/builder/src/routing/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Routes } from "react-router-dom";
+
+const Routing = () => {
+
+    return (
+        <Routes>
+
+
+        </Routes>
+    );
+};
+
+export default Routing;
